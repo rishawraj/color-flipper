05194c0f6386edf84f6a379990c711ee58bfd143
rishaw <rishawraj0703@gmail.com>
2022-02-10 20:17:15 +0530

    cool

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const letters = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
-// document.container.style.backgroundColor = "red";
 
 btn.addEventListener("click", function () {
   let appliedColor = "#";
   for (let i = 0; i < 6; i++) {
     appliedColor += letters[getRandomNumber()];
-    // console.log(appliedColor);
   }
-  //   console.log(appliedColor);
   color.textContent = appliedColor;
   document.body.style.backgroundColor = appliedColor;
 });
@@ -17,4 +14,3 @@ btn.addEventListener("click", function () {
 function getRandomNumber() {
   return Math.floor(Math.random() * letters.length);
 }
-// console.log(getRandomNumber());
